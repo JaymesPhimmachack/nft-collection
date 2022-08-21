7d757ed433eae7463eec55cf00706570faa0179b
Jaymes Phimmachack <phimmachack.jaymes@gmail.com>
2022-08-21 19:41:22 -0400

    add finish project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x4b0cc09447cd5f646ccd08ec0ce18788aab9bcc1";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
